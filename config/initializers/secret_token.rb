6189d1ef568e416beb5f5d68efbd8d6eeddbe6c0
ArielUmansky <umanskyariel@gmail.com>
2015-01-17 19:26:07 -0300

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xfici::Application.config.secret_token = '04b130fcad43331b2a6666af78007a0bc55653d382d8df56a6eff2d5b28c4ef88c93dc7ae94f31ac604185909e479d02efb88bb7e8c0a65b4438f1dde3609a16'
